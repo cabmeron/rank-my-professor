59499db18b5732726988ecf679301bb03c040020
cabmeron <116330722+cabmeron@users.noreply.github.com>
2025-07-12 00:30:12 -0700

    refactor: data loading

--- backend/src/data_loader.py
+++ backend/src/data_loader.py
@@ -4,11 +4,12 @@ from fastapi import HTTPException
 
 @cache
 def load_school_json_file(school_name):
+    
     try:
-        with open(f'{school_name}.json', 'r') as file:
+        with open(f'../data/colleges/unr/{school_name}.json', 'r') as file:
             return json.load(file)
     except FileNotFoundError:
-        raise HTTPException(status_code=404, detail="School data file")
+        raise HTTPException(status_code=404, detail="Error Loading School JSON FILE (File not found)")
         
     
 def get_school_departments_json(school_name):
